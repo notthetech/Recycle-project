1fe0194f15032aa08fd41f37b5d804ccbd81cb36
Llyween Hunt <llyweenhunt671@gmail.com>
2025-04-12 13:45:32 -0500

    Update main_code.c
    Filled out most of the other functions

--- main_code.c
+++ main_code.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
 
-/*
-When finished copy and paste finished code here
-*/
-
-// void Waste Categorization 
-// void Collection Schedule
+void WasteCategorization() // A checklist to see what to do with it
+{
+    int choice = 0;
+    printf("\n\nWaste Categorization:\n");
+    printf("Describe the waste:\n");
+    while(1)
+    {
+        printf("1. Organic\n");
+        printf("2. Inorganic\n");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            printf("Organic waste includes food scraps, yard waste, and paper products.\n");
+            printf("Look at your label and compost if possible.\n");
+        }
+        else if (choice == 2)
+        {
+            printf("Inorganic waste includes plastics, metals, and glass.\n");
+            printf("Look at your label and recycle if possible.\n");
+        }
+        else
+        {
+            printf("Invalid choice, please try again.\n");
+        }
+    }
+}
 
 void WasteSchedule() // check if choice is global or just local in main
 {
-    //code
+    int choice = 0;
+    printf("\n\nCollection Schedule:\n");
+    while (1)
+    {
+        printf("1. Collection Days\n");
+        printf("2. Collection Times\n");
+        printf("3. Back to Main Menu\n");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            //code for collection schedule calling from other files
+            default:
+                printf("Invalid, choose an option\n");
+        }
+    }
 }
 
 void UserTracking()// user tracking, code to create and remove user accounts
