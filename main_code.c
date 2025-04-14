4ec04170245d65504c7242a97de0bb1f8d303f9a
Llyween Hunt <llyweenhunt671@gmail.com>
2025-04-14 15:40:05 -0500

    Update main_code.c

--- main_code.c
+++ main_code.c
@@ -1,118 +1,23 @@
 #include <stdio.h>
+#include "functions.c"
+/*add more includes if needed
+don't forget to put functions in seperate files
+add more comments
+Use of File handling concepts, Structs, ADS
+*/
 
-void WasteCategorization() // A checklist to see what to do with it
+
+/*
+limit scanf to only take first char and then based off that
+*/
+int main()// add struct to include members and their points
 {
-    int choice = 0;
-    printf("\n\nWaste Categorization:\n");
-    printf("Describe the waste:\n");
-    while(1)
+    typedef struct Data
     {
-        printf("1. Organic\n");
-        printf("2. Inorganic\n");
-        printf("3. Back to Main Menu\n");
-        scanf("%d", &choice);
-        if (choice == 1)
-        {
-            printf("Organic waste includes food scraps, yard waste, and paper products.\n");
-            printf("Look at your label and compost if possible.\n");
-        }
-        else if (choice == 2)
-        {
-            printf("Inorganic waste includes plastics, metals, and glass.\n");
-            printf("Look at your label and recycle if possible.\n");
-        }
-        else if (choice == 3)
-        {
-            return;
-        }
-        else
-        {
-            printf("Invalid choice, please try again.\n");
-        }
-    }
-}
-
-void WasteSchedule() // check if choice is global or just local in main
-{
-    int choice = 0;
-    printf("\n\nCollection Schedule:\n");
-    while (1)
-    {
-        printf("1. Collection Days\n");
-        printf("2. Collection Times\n");
-        printf("3. Back to Main Menu\n");
-        scanf("%d", &choice);
-        switch(choice)
-        {
-            //code for collection schedule calling from other files
-            default:
-                printf("Invalid, choose an option\n");
-        }
-    }
-}
-
-void UserTracking()// user tracking, code to create and remove user accounts
-
-{
-    //code
-}
-//maybe add a ranking feature to the user tracking system
-
-void RewardSystem() // Reward System, find user points and redeem points
-{
-    int choice = 0;
-    printf("\n\nReward System:\n");
-    while (1)
-    {
-        printf("1. View Points\n");
-        printf("2. Redeem Points\n");
-        printf("3. Back to Main Menu\n");
-        scanf("%d", &choice);
-        switch(choice)
-        {
-            //code for reward system calling from other files
-            default:
-                printf("Invalid, choose an option\n");
-        }
-    }
-}
-
-void EduContent() // Educational Content
-{
-    int choice = 0;
-    printf("\n\nEducational Content:\n");
-    while (1)
-    {
-        printf("1. Waste Categories\n");
-        printf("2. Recycling Process\n");
-        printf("3. Composting\n");
-        printf("4. Waste Reduction Tips\n");
-        printf("5. Back to Main Menu\n");
-        scanf("%d", &choice);
-        switch (choice)
-        {
-            case 1:
-                printf("Info");
-                break;
-            case 2:
-                printf("Info");
-                break;
-            case 3:
-                printf("Info");
-                break;
-            case 4:
-                printf("Info");
-                break;
-            case 5:
-                return;
-            default:
-                printf("Invalid, choose an option\n");
-        }
-    }
-}
-
-int main()
-{
+        char name[50];
+        int points;
+    } user;
+    //figure out how to make them as pointers and be used
     int choice = 0;
     printf("Welcome to the Waste Management System!\n");
     printf("Please select an option:\n");
